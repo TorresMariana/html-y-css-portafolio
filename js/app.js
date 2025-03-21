1834fd408a6e9a23a7547669b1bb537b4433f6d9
TorresMariana <mt.grafico@gmail.com>
2025-03-21 19:08:22 -0300

    creacion de la pagina about.html

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.querySelector('link[rel="stylesheet"]').setAttribute('href', 'css/style2.css');
+
+/*selecciona el primer elemento <link> que tiene el atributo rel igual a 
+"stylesheet", y luego cambia el valor de su atributo href a "css/style2.css", 
+lo que hace que se cargue un nuevo archivo de estilos.
+*/
+
+
+
+
+
