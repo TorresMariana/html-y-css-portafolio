b5e110208fa67d61c769a8beb849c2f95dd81adc
TorresMariana <mt.grafico@gmail.com>
2025-03-23 00:09:56 -0300

    se agrego estilo formal

--- js/app.js
+++ js/app.js
@@ -35,12 +35,34 @@ function menuHamburguesa(){
 
 
 
-function cambiarArchivoCSS(estilo, event) {
-    // Prevenir la acción por defecto de hacer clic en el enlace
-    event.preventDefault();
 
-    // Cambiar el archivo CSS actual por el nuevo.
-    let link = document.getElementById('stylesheet__link');
-    link.setAttribute('href', 'css/' + estilo);
-}
+
+document.addEventListener('DOMContentLoaded', function(){
+    function cambiarArchivoCSS(estilo, event) {
+        // Prevenir la acción por defecto de hacer clic en el enlace
+        event.preventDefault();
+
+        // Guardar la selección en localStorage
+        localStorage.setItem('selectedStyle', estilo);
+        console.log(estilo);
+
+        // Cambiar el archivo CSS actual por el nuevo.
+        let link = document.getElementById('stylesheet__link');
+        link.setAttribute('href', 'css/' + estilo);
+    }
+    window.cambiarArchivoCSS = cambiarArchivoCSS;
+
+    // Al cargar la página, aplicar el estilo guardado en localStorage
+    // Revisar si hay un estilo guardado en localStorage
+    let estiloGuardado = localStorage.getItem('selectedStyle');
+    console.log(estiloGuardado);
+    if (estiloGuardado) {
+        let link = document.getElementById('stylesheet__link');
+        if (link) {
+            // Cambiar el archivo CSS según lo guardado
+            link.setAttribute('href', 'css/' + estiloGuardado);
+        }
+    }
+});
+
 
